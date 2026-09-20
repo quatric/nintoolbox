b770e0fdd1debd6edfb46900664fa2a8f151c0ef
quatric <quatricsoftware@gmail.com>
2026-09-19 23:21:30 -0500

    tests: fix Avalanche texture fixture header size

--- tests/mk_avtex.py
+++ tests/mk_avtex.py
@@ -5,8 +5,8 @@ import os, struct, sys
 
 pix = struct.pack('>H', 0xFFFF) * 64  # opaque white
 thb = struct.pack('>13I', 1, 0x10, 0, len(pix), len(pix), len(pix) + 0x20, len(pix), 0, 0, 0, 0, 0, 0)[:0x1c]
-thb += struct.pack('>HHHH', 8, 8, 5, 1) + struct.pack('>HHIII', 8, 8, 1, len(pix), 0)
-thb = thb.ljust(48, b'\0')
+thb += struct.pack('>HHHH', 8, 8, 5, 1) + struct.pack('>HHII', 8, 8, 1, len(pix))
+assert len(thb) == 48, len(thb)
 os.makedirs(sys.argv[1], exist_ok=True)
 open(os.path.join(sys.argv[1], 'tex.thb'), 'wb').write(thb)
 open(os.path.join(sys.argv[1], 'tex.tbb'), 'wb').write(pix)
